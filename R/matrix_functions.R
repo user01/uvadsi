87a06404f53dfbf0ce5670da2b7b54da326fd911
Erik <erik.langenborg@gmail.com>
2016-10-18 20:12:14 -0400

    Added function to get rows on a matrix

--- R/matrix_functions.R
+++ R/matrix_functions.R
@@ -13,3 +13,18 @@
 get_col <- function(.m, cols) {
   .m[, cols]
 }
+
+
+#' Select rows by indices
+#'
+#' @param .m A 2d matrix
+#' @param cols Numeric or character vector or empty (missing)
+#' @return Subset of rows of matrix.
+#' @examples
+#' get_row(matrix(1:6, nrow = 3), 2)
+#' get_row(matrix(1:6, nrow = 3), c(2,3))
+#' get_row(matrix(1:6, nrow = 3, byrow = TRUE, dimnames = list(c("a", "b", "c"), c("x", "y"))), "b")
+#' @export
+get_row <- function(.m, rows) {
+  .m[rows, ]
+}

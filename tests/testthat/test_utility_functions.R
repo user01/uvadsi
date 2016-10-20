d54aacc74d2cac7e97ad6308309890e98f409f83
Erik <erik.langenborg@gmail.com>
2016-10-20 10:39:52 -0400

    Tests for if_else function

--- tests/testthat/test_utility_functions.R
+++ tests/testthat/test_utility_functions.R
@@ -19,3 +19,36 @@ test_that("get_ error", {
         get_("c")
     }, "object 'c' not found")
 })
+
+# #############################################################################
+# if_else Tests
+# #############################################################################
+test_that("if_else simple", {
+
+  1:10 %>%
+    if_else(T, sum, mean) %>%
+    expect_equal(sum(1:10))
+
+  1:10 %>%
+    if_else(F, sum, mean) %>%
+    expect_equal(mean(1:10))
+})
+
+test_that("if_else compound", {
+
+  1:10 %>%
+    if_else(., length(.) > 4, sum, mean) %>%
+    expect_equal(sum(1:10))
+
+  1:10 %>%
+    if_else(., length(.) < 4, sum, mean) %>%
+    expect_equal(mean(1:10))
+
+  1:10 %>%
+    if_else(., length(.) %% 2 == 0, first, last) %>%
+    expect_equal(1)
+
+  1:11 %>%
+    if_else(., length(.) %% 2 == 0, first, last) %>%
+    expect_equal(11)
+})

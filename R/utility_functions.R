cd4fa3d8d27212ba1be52644c112945bd60abbdc
Erik <erik.langenborg@gmail.com>
2016-10-20 09:59:23 -0400

    Added if_else function for piping logic

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -5,11 +5,29 @@
 #' Flips the order of the {base} get()
 #'
 #' @param .o An object
-#' @param name Character vector
+#' @param name Character vector (or castable)
 #' @return Object
 #' @examples
 #' get_(list(b = 4:2), 'b')
 #' @export
 get_ <- function(.o, name) {
   get(name, .o)
+}
+
+
+#' Apply logic based on logical
+#'
+#' If truth is truthy, applies lhs to data. Else, apply rhs to data
+#'
+#' @param .data Any value
+#' @param .truth Logical Value
+#' @param .lhs A function
+#' @param .rhs A function
+#' @return Any
+#' @examples
+#' if_else(1:5, T, sum, mean)
+#' if_else(1:5, F, sum, mean)
+#' @export
+if_else <- function(.data, .truth, .lhs, .rhs) {
+  if (.truth) { .lhs(.data) } else { .rhs(.data) }
 }

3b35dc14f7395b9a1b66d6d8371f04698f8e17de
Erik <erik.langenborg@gmail.com>
2016-11-11 18:35:31 -0500

    Increased test suite coverage

--- tests/testthat/test_even_or_odd_functions.R
+++ tests/testthat/test_even_or_odd_functions.R
@@ -8,4 +8,22 @@ test_that("even tests", {
   even_or_odd(1:4) %>%
     expect_equal("Even")
 
+  # 1 2 3 has 3 elements, but one even number, so this is also even
+  even_or_odd(1:3) %>%
+    expect_equal("Even")
+
+  # 1 2 3 4 5 has 5 elements, but both 2 and 4 are even, so this is even
+  even_or_odd(1:5) %>%
+    expect_equal("Even")
+
+  # 2 3 4 has 3 elements, but both 2 and 4 are even, so this is even
+  even_or_odd(2:4) %>%
+    expect_equal("Even")
+
+})
+
+test_that("odd tests", {
+  # 1 has only 1 element and no even elements, so this should be odd
+  even_or_odd(1) %>%
+    expect_equal("Odd")
 })
